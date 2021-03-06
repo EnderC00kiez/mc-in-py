0c3683700acaf2ce7264884ecaba6b0c3cfbb247
EnderC00kiez <EnderC00kiez@users.noreply.github.com>
2021-03-06 13:23:36 -0500

    Added Program

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-import click
-import requests
+import os
+try:
+    import click
+except:
+    os.system("pip3 install click")
+    import click
+try:
+    import requests
+except:
+    os.system("pip3 install requests")
+    import requests
 click.echo("Contacting McInPy servers...")
-remotescript = requests.get("https://raw.githubusercontent.com/EnderC00kiez/mc-in-py/master/main.py")
-with open("main.py", "r") as main:
-    if remotescript.text == main.read():
-        click.echo("You are on the lastest version.")
-    else:
-        click.echo("You are on an older version then the servers.")
-        if input("Update to lastest version? Type Y to update and any other key to not.") == "Y" or "y":
-            click.echo("Updating...")
-            print(remotescript.text)
+servers = requests.get("https://raw.githubusercontent.com/EnderC00kiez/mc-in-py/master/main.py")
+if str(servers) != "<Response [200]>":
+    print("Server Error (Status Code " + str(servers) + ")")
+elif str(servers) == "<Response [200]>":
+    print("Servers contacted.")
+def kill():
+    exit()
+while True:
+    eval(input(">>> "))
